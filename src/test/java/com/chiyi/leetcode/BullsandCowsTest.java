45ab50843adefc781b8d3f57f1f5b8f06074f7d1
chyi0425 <chiyi@douyu.tv>
2018-06-07 16:55:53 +0800

    add

--- src/test/java/com/chiyi/leetcode/BullsandCowsTest.java
+++ src/test/java/com/chiyi/leetcode/BullsandCowsTest.java
@@ -17,9 +17,22 @@ public class BullsandCowsTest {
 
     @Test
     public void getHint() throws Exception {
-        assertEquals("1A0B",new BullsandCows().getHint("11","10"));
-        assertEquals("1A2B",new BullsandCows().getHit2("3286","5826"));
-        assertEquals("1A2B",new BullsandCows().getHint2("3286","5826"));
+        assertEquals("1A0B", new BullsandCows().getHint("11", "10"));
+        assertEquals("1A2B", new BullsandCows().getHit2("3286", "5826"));
+        assertEquals("1A2B", new BullsandCows().getHint2("3286", "5826"));
+    }
+
+    @Test
+    public void test() throws Exception {
+        TTTT t1 = new TTTT();
+        TTTT t2 = new TTTT();
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t1 = t2);
+    }
+
+    private static class TTTT{
+
     }
 
 }
