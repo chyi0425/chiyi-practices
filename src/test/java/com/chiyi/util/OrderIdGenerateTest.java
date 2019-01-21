91bcb309b5274a8760349cdd7cb9dee2bbb6a9c3
chyi0425 <chiyi@douyu.tv>
2019-01-21 20:30:42 +0800

    add

--- src/test/java/com/chiyi/util/OrderIdGenerateTest.java
+++ src/test/java/com/chiyi/util/OrderIdGenerateTest.java
@@ -3,6 +3,8 @@ package com.chiyi.util;
 import com.google.common.math.LongMath;
 import org.junit.Test;
 
+import java.util.Random;
+
 /**
  * @author chiyi
  * @date 2019/1/18.
@@ -10,13 +12,50 @@ import org.junit.Test;
 public class OrderIdGenerateTest {
 
     @Test
-    public void generateConsumeOrderId() {
-        System.out.println(OrderIdGenerate.generateConsumeOrderId(1,1,System.currentTimeMillis()));
-        System.out.println(OrderIdGenerate.generateConsumeOrderId(1,1,System.currentTimeMillis()));
-        System.out.println(OrderIdGenerate.generateConsumeOrderId(1,1,System.currentTimeMillis()));
-        System.out.println(OrderIdGenerate.generateConsumeOrderId(1,1,System.currentTimeMillis()));
-//        System.out.println(OrderIdGenerate.generateConsumeOrderId(1,1,System.currentTimeMillis()));
-//        System.out.println(OrderIdGenerate.generateConsumeOrderId(1,1,System.currentTimeMillis()));
-        System.out.println(LongMath.checkedPow(10,18));
+    public void generateConsumeOrderId() throws InterruptedException {
+        int x = 1;
+        int n1 = 5;
+        int n2 = 6;
+        int n3 = 7;
+        int m1 = 0;
+        int m2 = 0;
+        int m3 = 0;
+        Random random = new Random(3);
+        while (true) {
+            Thread.sleep(1000);
+            int r = random.nextInt(3);
+            if (r == 0 && flag(m1, n1, x)) {
+                System.out.println("m1++");
+                m1++;
+                System.out.println(m1);
+                continue;
+            }
+            if (r == 1 && flag(m2, n2, x)) {
+                System.out.println("m2++");
+                m2++;
+                System.out.println(m2);
+                continue;
+            }
+            if (r == 2 && flag(m3, n3, x)) {
+                System.out.println("m3++");
+                m3++;
+                System.out.println(m3);
+                continue;
+            }
+            if ((!flag(m3, n3, x)) && (!flag(m1, n1, x)) && (!flag(m2, n2, x))) {
+                System.out.println("------------------------------------");
+                x++;
+                System.out.println("x++");
+                System.out.println(x);
+            }
+        }
+    }
+
+    public boolean flag(int m, int n, int x) {
+        if (m % (x * n + 1) == n * x) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
