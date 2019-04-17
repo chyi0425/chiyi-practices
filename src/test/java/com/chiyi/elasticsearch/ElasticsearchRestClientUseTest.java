333ebd7e3fbdaeeae0158023caf5643a9ba34efe
chyi0425 <chiyi@douyu.tv>
2019-04-17 17:07:18 +0800

    add

--- src/test/java/com/chiyi/elasticsearch/ElasticsearchRestClientUseTest.java
+++ src/test/java/com/chiyi/elasticsearch/ElasticsearchRestClientUseTest.java
@@ -1,18 +1,14 @@
 package com.chiyi.elasticsearch;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import net.minidev.json.JSONUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.entity.ContentType;
@@ -23,6 +19,8 @@ import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.client.RestClient;
 import org.junit.Before;
 import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import redis.clients.jedis.Jedis;
 import us.codecraft.webmagic.selector.Json;
 
 import static org.junit.Assert.*;
@@ -32,7 +30,7 @@ import static org.junit.Assert.*;
  */
 public class ElasticsearchRestClientUseTest {
 
-    private String endpoint = "/test-ocean-doc/docs/";
+    private String endpoint = "/janus-report-all";
     private static final String METHOD_GET = "GET";
     private static final String METHOD_PUT = "PUT";
     private static final String METHOD_POST = "POST";
@@ -42,7 +40,7 @@ public class ElasticsearchRestClientUseTest {
     @Before
     public void setUp() throws Exception {
 //        restClient = RestClient.builder(new HttpHost("10.18.3.167", 9200, "http")).build();
-        restClient = RestClient.builder(new HttpHost("10.18.3.27", 9201, "http")).build();
+        restClient = RestClient.builder(new HttpHost("10.32.99.119", 9200, "http")).build();
     }
 
     @Test
@@ -79,7 +77,7 @@ public class ElasticsearchRestClientUseTest {
 
     @Test
     public void getDocument() throws Exception {
-        String endpoint = "/test-ocean-doc/docs/1006";
+        String endpoint = "/janus-access";
         try {
             Response response = restClient.performRequest(METHOD_GET, endpoint);
             System.out.println(response);
@@ -99,18 +97,102 @@ public class ElasticsearchRestClientUseTest {
     @Test
     public void QueryAll() throws Exception {
         HashMap<String, String> paramMap = new HashMap<String, String>();
-        paramMap.put("content", "我看");
-        String endpoint = "/Mobile_hot_recom_201802/docs/_search";
+        paramMap.put("start_date", "1553961600000");
+//        paramMap.put("host", "athena.biz.ocean-bp.com");
+        String endpoint = "/janus-report-all/_search";
         JSONObject parameter = new JSONObject();
         JSONObject query = new JSONObject();
         query.put("match_phrase", paramMap);
         parameter.put("query", query);
-        parameter.put("size", 20);
+        parameter.put("size", 10);
         HttpEntity entity = new NStringEntity(parameter.toJSONString(), ContentType.APPLICATION_JSON);
 
         Response response = restClient
-                .performRequest(METHOD_POST, endpoint, Collections.<String, String>emptyMap(), entity);
+                .performRequest(METHOD_GET, endpoint, Collections.<String, String>emptyMap(), entity);
         System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void queryJanus() throws IOException {
+        Calendar calendar = Calendar.getInstance();
+        Date date = new Date();
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        String endpoint = "/janus-report-all/_search";
+        JSONObject parameter = new JSONObject();
+        JSONObject query = new JSONObject();
+//        JSONObject bool = new JSONObject();
+//        JSONObject must = new JSONObject();
+//        bool.put("must",must);
+        JSONObject match = new JSONObject();
+        parameter.put("query", query);
+        query.put("match", match);
+        match.put("start_date", calendar.getTimeInMillis());
+        HttpEntity entity = new NStringEntity(parameter.toJSONString(), ContentType.APPLICATION_JSON);
+        Response response = restClient.performRequest(METHOD_POST, "/janus-report-all/_search", Collections.<String, String>emptyMap(), entity);
+        String result = EntityUtils.toString(response.getEntity());
+        JSONObject map = JSONObject.parseObject(result);
+        JSONObject jsonObject = (JSONObject) map.get("hits");
+        int total = (Integer) jsonObject.get("total");
+        parameter.put("size", total);
+        entity = new NStringEntity(parameter.toJSONString(), ContentType.APPLICATION_JSON);
+        response = restClient.performRequest(METHOD_POST, "/janus-report-all/_search", Collections.<String, String>emptyMap(), entity);
+        result = EntityUtils.toString(response.getEntity());
+        map = JSONObject.parseObject(result);
+        jsonObject = (JSONObject) map.get("hits");
+        total = (Integer) jsonObject.get("total");
+        JSONArray jsonArray = (JSONArray) jsonObject.get("hits");
+        final Iterator<Object> iterator = jsonArray.iterator();
+        Map<String, Map<String, Long>> hostMap = new HashMap<>();
+        while (iterator.hasNext()) {
+
+            JSONObject nextObject = (JSONObject) iterator.next();
+            JSONObject source = (JSONObject) nextObject.get("_source");
+            if (source == null) {
+                continue;
+            }
+            String invokeUri = source.getString("uri");
+            if ("/".equals(invokeUri)){
+                continue;
+            }
+            String host = source.getString("host");
+            Long invokeTimes = source.getLong("total");
+            String[] uriArr = invokeUri.split("/");
+            String module = uriArr[1];
+            Map<String, Long> moduleMap = hostMap.get(host);
+            if (moduleMap == null) {
+                moduleMap = new HashMap<>();
+            }
+            Long alreadInvokeTime = moduleMap.get(module);
+            if (alreadInvokeTime == null) {
+                alreadInvokeTime = invokeTimes;
+            } else {
+                alreadInvokeTime += invokeTimes;
+            }
+            moduleMap.put(module, alreadInvokeTime);
+            hostMap.put(host, moduleMap);
+        }
+        System.out.println(jsonArray.size());
+        System.out.println(total);
+    }
+
+    @Test
+    public void getValueFromJsonObj() {
+//        Calendar calendar = Calendar.getInstance();
+//        Date date = new Date();
+//        calendar.setTime(date);
+//        calendar.add(Calendar.DAY_OF_YEAR, -1);
+//        calendar.set(Calendar.HOUR_OF_DAY, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
+//        calendar.set(Calendar.MILLISECOND, 0);
+//        System.out.println(calendar.getTimeInMillis());
+
+        String s1 = "12345678912345678912";
+        System.out.println(s1.substring(0,20));
     }
 
     public void queryDocsByCateId(String cateId, int limit) throws Exception {
@@ -165,16 +247,16 @@ public class ElasticsearchRestClientUseTest {
         String value = "是肯德<em class='highlight'>酒</em>基快捷酒店今年我看就是<em class='highlight'>看就</em>肯德基快捷酒店今年我看就是肯德基快捷酒店今年我看就是肯德是肯德基快捷酒店今年我看就是肯德基快捷酒店今年我看就是肯德基快捷酒店今年我看就是肯德基快捷酒店今年我看就是肯德基快捷酒店今年<em class='highlight'>你</em><em class='highlight'>你</em><em class='highlight'>你</em><em class='highlight'>你</em>们我来咯我肯德基快捷酒是肯德基快捷酒店今年我看就是肯德基快捷酒店今年我看就是肯德基快捷酒店今年我看就是肯德是肯德基快捷酒";
 
 
-        Map<Integer,Integer> hightlightTagBeginMap = countStringSub(value,"<em class='highlight'>");
-        Map<Integer,Integer> hightlightTagEndMap = countStringSub(value,"</em>");
+        Map<Integer, Integer> hightlightTagBeginMap = countStringSub(value, "<em class='highlight'>");
+        Map<Integer, Integer> hightlightTagEndMap = countStringSub(value, "</em>");
         Set<String> hightlightSet = new HashSet<String>();
         Set<String> withoutHightlightSet = new HashSet<String>();
-        Map<String,String> highlightMap = new HashMap<>();
+        Map<String, String> highlightMap = new HashMap<>();
         int highlightSize = hightlightTagBeginMap.size();
-        for(int i=1;i<=highlightSize;i++){
-            highlightMap.put(value.substring(hightlightTagBeginMap.get(i)+22,hightlightTagEndMap.get(i)),value.substring(hightlightTagBeginMap.get(i),hightlightTagEndMap.get(i)+5));
-            hightlightSet.add(value.substring(hightlightTagBeginMap.get(i),hightlightTagEndMap.get(i)+5));
-            withoutHightlightSet.add(value.substring(hightlightTagBeginMap.get(i)+22,hightlightTagEndMap.get(i)));
+        for (int i = 1; i <= highlightSize; i++) {
+            highlightMap.put(value.substring(hightlightTagBeginMap.get(i) + 22, hightlightTagEndMap.get(i)), value.substring(hightlightTagBeginMap.get(i), hightlightTagEndMap.get(i) + 5));
+            hightlightSet.add(value.substring(hightlightTagBeginMap.get(i), hightlightTagEndMap.get(i) + 5));
+            withoutHightlightSet.add(value.substring(hightlightTagBeginMap.get(i) + 22, hightlightTagEndMap.get(i)));
         }
         System.out.println(hightlightSet);
         System.out.println(withoutHightlightSet);
@@ -182,17 +264,17 @@ public class ElasticsearchRestClientUseTest {
         int valueLength = value.length();
         int firstEm = value.indexOf("<em");
 
-        Iterator<Entry<String,String>> highlightMapIterator = highlightMap.entrySet().iterator();
-        while (highlightMapIterator.hasNext()){
-            Entry<String,String> entry = highlightMapIterator.next();
-            value = value.replaceAll(entry.getValue(),entry.getKey());
+        Iterator<Entry<String, String>> highlightMapIterator = highlightMap.entrySet().iterator();
+        while (highlightMapIterator.hasNext()) {
+            Entry<String, String> entry = highlightMapIterator.next();
+            value = value.replaceAll(entry.getValue(), entry.getKey());
         }
 
         int beginIndex;
         int endIndex;
         if (valueLength - firstEm < 50) {
             endIndex = valueLength;
-            beginIndex = valueLength -100;
+            beginIndex = valueLength - 100;
         } else if (firstEm > 50) {
             beginIndex = firstEm - 50;
             endIndex = firstEm + 50;
@@ -201,25 +283,35 @@ public class ElasticsearchRestClientUseTest {
             endIndex = 110;
         }
         String newValue = value.substring(beginIndex, endIndex);
-        Iterator<Entry<String,String>> iterator = highlightMap.entrySet().iterator();
-        while (iterator.hasNext()){
-            Entry<String,String> entry = iterator.next();
-            newValue = newValue.replaceAll(entry.getKey(),entry.getValue());
+        Iterator<Entry<String, String>> iterator = highlightMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Entry<String, String> entry = iterator.next();
+            newValue = newValue.replaceAll(entry.getKey(), entry.getValue());
         }
         System.out.println(firstEm);
         System.out.println(newValue);
     }
 
-    public  Map<Integer,Integer> countStringSub(String content, String subStr) {
+    public Map<Integer, Integer> countStringSub(String content, String subStr) {
         int index = 0;
         int count = 0;
         int fromIndex = 0;
-        Map<Integer,Integer> countPositionMap = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> countPositionMap = new HashMap<Integer, Integer>();
         while ((index = content.indexOf(subStr, fromIndex)) != -1) {
             fromIndex = index + subStr.length();
             count++;
-            countPositionMap.put(count,index);
+            countPositionMap.put(count, index);
         }
         return countPositionMap;
+    }
+
+    @Test
+    public void testBrpop(){
+        Jedis jedis = new Jedis("10.1.104.13",6740);
+        List<String> strings = jedis.brpop(0,"test_video_danma");
+        System.out.println(strings.size());
+        for (String s:strings){
+            System.out.println(s);
+        }
     }
 }
