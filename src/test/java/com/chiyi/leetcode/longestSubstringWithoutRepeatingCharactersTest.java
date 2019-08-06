44b7311278fac331cc091d444892954e261fb9c0
chiyi <chyi0425@yeah.net>
2019-08-06 22:04:34 +0800

    :

---
+++ src/test/java/com/chiyi/leetcode/longestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,14 @@
+package com.chiyi.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class longestSubstringWithoutRepeatingCharactersTest {
+
+    @Test
+    public void lengthOfLongestSubstring() {
+        longestSubstringWithoutRepeatingCharacters l = new longestSubstringWithoutRepeatingCharacters();
+        System.out.println(l.lengthOfLongestSubstring("pwwkew"));
+    }
+}
