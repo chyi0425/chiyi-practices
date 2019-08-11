dba94ba6c3450d7bc394de329d2283707e0008d6
chiyi <chyi0425@yeah.net>
2019-08-11 17:03:27 +0800

    dwd

--- src/main/java/com/chiyi/leetcode/Solution.java
+++ src/main/java/com/chiyi/leetcode/Solution.java
@@ -5,6 +5,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
+
+    /**
+     * given two arrays,write a function to compute their intersection.
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> nums1Set = new HashSet<>(nums1.length);
         Set<Integer> nums2Set = new HashSet<>(nums2.length);
@@ -23,5 +31,29 @@ public class Solution {
             }
         }
         return Arrays.copyOf(result, idx);
+    }
+
+
+    /**
+     * 7
+     * given a 32-bit signed integer,reverse digits of an integer.
+     *
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x = x / 10;
+//            -2147483648
+            // if result*10+pop > INT_MAX (2147483647) result>= INT_MAX/10
+            if ((result > Integer.MAX_VALUE/10|| (result == Integer.MAX_VALUE/10 && pop > 7)) ||
+                    (result < Integer.MIN_VALUE/10 || (result == Integer.MIN_VALUE/10 && pop < -8))) {
+                return 0;
+            }
+            result = result * 10 + pop;
+        }
+        return result;
     }
 }
---
+++ src/test/java/com/chiyi/leetcode/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.chiyi.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    private Solution solution = new Solution();
+
+    @Test
+    public void intersection() {
+    }
+
+    @Test
+    public void reverse() {
+        System.out.println(solution.reverse(1534236469));
+    }
+}
