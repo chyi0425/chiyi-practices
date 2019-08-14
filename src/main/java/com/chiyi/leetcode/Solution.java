d9022c08f8b54ce581f27be59bfbf0852202ddc9
chi yi <chyi0425@yeah.net>
2019-08-14 09:56:42 +0800

    Update Solution.java

--- src/main/java/com/chiyi/leetcode/Solution.java
+++ src/main/java/com/chiyi/leetcode/Solution.java
@@ -56,4 +56,34 @@ public class Solution {
         }
         return result;
     }
+    
+    
+   /**
+     * 206
+     * reverse a singly likned list.
+     *
+     * @param x
+     * @return
+     */
+        public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
 }
