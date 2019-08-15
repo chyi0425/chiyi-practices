836dcbbb45aacc9b48dce4a233ee516c381761d6
chi yi <chyi0425@yeah.net>
2019-08-15 14:05:09 +0800

    Update Solution.java

--- src/main/java/com/chiyi/leetcode/Solution.java
+++ src/main/java/com/chiyi/leetcode/Solution.java
@@ -86,4 +86,24 @@ public class Solution {
         head.next = null;
         return p;
     }
+    
+     /**
+     * 9
+     * determine whether an integer is a palindrome.An integer is a palindrome when it reads the same backward as forward.
+     *
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
 }
